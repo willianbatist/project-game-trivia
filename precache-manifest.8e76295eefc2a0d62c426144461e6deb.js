0302d9a2a53b5a0b6cf46a1ead44b946ccc4146e
willianbatist <willianbatist@users.noreply.github.com>
2022-02-18 01:26:35 +0000

    deploy: 73615c8e7e1bc16d9dd6efd4b27c948f2a12427d

---
+++ precache-manifest.8e76295eefc2a0d62c426144461e6deb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b308bfce32a511921f50facd2f0c8743",
+    "url": "/project-game-trivia/index.html"
+  },
+  {
+    "revision": "6ee2ee4e49b331c433a7",
+    "url": "/project-game-trivia/static/css/main.bf602b90.chunk.css"
+  },
+  {
+    "revision": "1d518b35589dbae0cfae",
+    "url": "/project-game-trivia/static/js/2.5ee9864c.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/project-game-trivia/static/js/2.5ee9864c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ee2ee4e49b331c433a7",
+    "url": "/project-game-trivia/static/js/main.64a64528.chunk.js"
+  },
+  {
+    "revision": "dfdfed885096310ebd9c",
+    "url": "/project-game-trivia/static/js/runtime-main.c9bf09ae.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/project-game-trivia/static/media/trivia.466d153e.png"
+  }
+]);
