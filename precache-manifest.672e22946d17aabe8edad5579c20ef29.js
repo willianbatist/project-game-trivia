7bd3ca2e57dc948a30b6fd9a2ab548888295dd8d
willianbatist <willianbatist@users.noreply.github.com>
2022-02-18 00:25:59 +0000

    deploy: ca9ba8b8de67bad50558d3cde1ae4eafc7bd2011

---
+++ precache-manifest.672e22946d17aabe8edad5579c20ef29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3007553aadb9ffc245d8ce761b503c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be6e170885caf8d83ab3",
+    "url": "/static/css/main.bf602b90.chunk.css"
+  },
+  {
+    "revision": "c9b1ab6d0ccefe2e839e",
+    "url": "/static/js/2.13399ef1.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.13399ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be6e170885caf8d83ab3",
+    "url": "/static/js/main.491f5aec.chunk.js"
+  },
+  {
+    "revision": "f8a4e7c1de7ec49c0042",
+    "url": "/static/js/runtime-main.e2209b7b.js"
+  },
+  {
+    "revision": "466d153e266e6e813c5ef2c55aa5fc2c",
+    "url": "/static/media/trivia.466d153e.png"
+  }
+]);
